6674e1b8c92680c5cdd3ba9362f5a6bf7b992a04
KosM <mpoukouvalas.kk@gmail.com>
2022-10-01 15:27:55 +0300

    chore: add husky, eslint, prettier,jest,lint-staged,github actions for publish realeases , commitizen
    setup all configs needed for automation in tests code formating / linting on staged files before
    each commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,15 @@
 module.exports = {
-  env: {
-    commonjs: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parserOptions: {
-    ecmaVersion: 12,
-  },
-  rules: {
-    'max-len': ['error', { code: 140 }],
-  },
+    env: {
+        commonjs: true,
+        es2021: true,
+        node: true,
+        jest: true,
+    },
+    extends: ['airbnb-base', 'prettier'],
+    parserOptions: {
+        ecmaVersion: 12,
+    },
+    rules: {
+        'max-len': ['error', { code: 140 }],
+    },
 };
